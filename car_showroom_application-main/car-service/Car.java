a439b01f5bc810b52eb770924ac7872848ab9a16
ahmedazzam991 <zerbracobra991@gmail.com>
2024-06-10 16:12:37 +0300

    Third commit

---
+++ car_showroom_application-main/car-service/Car.java
@@ -0,0 +1,19 @@
+package com.example.carservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String make;
+    private String model;
+    private double price;
+
+    // Getters and Setters
+}
+
