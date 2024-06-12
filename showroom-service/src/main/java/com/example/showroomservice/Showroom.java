a7638b7d1650c40ff8c58d6c2d51e108fffa1f4e
ahmedazzam991 <zebracobra991@gmail.com>
2024-06-13 01:20:14 +0300

    Second commit

--- showroom-service/src/main/java/com/example/showroomservice/Showroom.java
+++ showroom-service/src/main/java/com/example/showroomservice/Showroom.java
@@ -1,10 +1,17 @@
 package com.example.showroomservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Showroom {
     @Id
