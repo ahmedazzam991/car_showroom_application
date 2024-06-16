dbb2d25c8f46b18d9e995b9090be8d3714757354
ahmedazzam991 <zebracobra991@gmail.com>
2024-06-17 00:19:32 +0300

    Fifth commit

--- car-showroom-frontend/src/main/java/com/example/carshowroomfrontend/IndexController.java
+++ car-showroom-frontend/src/main/java/com/example/carshowroomfrontend/IndexController.java
@@ -1,12 +1,16 @@
 package com.example.carshowroomfrontend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -20,8 +24,22 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        List cars = restTemplate.getForObject("http://car-service/cars", List.class);
-        List showrooms = restTemplate.getForObject("http://showroom-service/showrooms", List.class);
+        ResponseEntity<List<Car>> carResponse = restTemplate.exchange(
+            "http://car-service/cars",
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<List<Car>>() {}
+        );
+
+        ResponseEntity<List<Showroom>> showroomResponse = restTemplate.exchange(
+            "http://showroom-service/showrooms",
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<List<Showroom>>() {}
+        );
+
+        List<Car> cars = carResponse.getBody();
+        List<Showroom> showrooms = showroomResponse.getBody();
 
         logger.info("Cars: " + cars);
         logger.info("Showrooms: " + showrooms);
@@ -30,18 +48,44 @@ public class IndexController {
         model.addAttribute("showrooms", showrooms);
         model.addAttribute("search", new SearchForm());
         model.addAttribute("searchResults", null);
+        model.addAttribute("price", null);
 
         return "index";
     }
 
     @GetMapping("/search")
     public String search(@RequestParam String make, @RequestParam String model, Model modelAttr) {
-        List searchResults = restTemplate.getForObject("http://car-service/cars/search?make=" + make + "&model=" + model, List.class);
+        ResponseEntity<List<Car>> searchResponse = restTemplate.exchange(
+            "http://car-service/cars/search?make=" + make + "&model=" + model,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<List<Car>>() {}
+        );
 
-        modelAttr.addAttribute("cars", restTemplate.getForObject("http://car-service/cars", List.class));
-        modelAttr.addAttribute("showrooms", restTemplate.getForObject("http://showroom-service/showrooms", List.class));
+        List<Car> searchResults = searchResponse.getBody();
+
+        modelAttr.addAttribute("cars", restTemplate.exchange(
+            "http://car-service/cars",
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<List<Car>>() {}
+        ).getBody());
+
+        modelAttr.addAttribute("showrooms", restTemplate.exchange(
+            "http://showroom-service/showrooms",
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<List<Showroom>>() {}
+        ).getBody());
+
         modelAttr.addAttribute("search", new SearchForm(make, model));
         modelAttr.addAttribute("searchResults", searchResults);
+
+        if (searchResults != null && !searchResults.isEmpty()) {
+            modelAttr.addAttribute("price", searchResults.get(0).getPrice());
+        } else {
+            modelAttr.addAttribute("price", "Car not found");
+        }
 
         return "index";
     }
--- showroom-service/src/main/java/com/example/showroomservice/ShowroomRepository.java
+++ showroom-service/src/main/java/com/example/showroomservice/ShowroomRepository.java
@@ -1,7 +1,9 @@
 package com.example.showroomservice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShowroomRepository extends JpaRepository<Showroom, Long> {
 }
 
